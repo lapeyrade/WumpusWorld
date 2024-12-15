a5ccfa60a354bcc674aedab197c2a541817f5215
Sylvain Lapeyrade <sylvain.lapeyrade@hotmail.fr>
2024-12-16 02:30:27 +0900

    Optimize localization.pl by combining object/cell and danger/cell checks for efficiency, and refactor adjacent_cell predicate using delta positions for improved clarity and performance.

--- Assets/StreamingAssets/localization.pl
+++ Assets/StreamingAssets/localization.pl
@@ -1,30 +1,27 @@
 :- discontiguous data_concept/2.
 
 /*** Localization rules***/
+% Combined object/cell check for efficiency
 distance([Elem1, [X1, Y1]], [Elem2, [X2, Y2]]):-
-    (
-        data_concept([Elem2, [X2, Y2]], object);
-        data_concept([Elem2, [X2, Y2]], cell)
-    ),
+    data_concept([Elem2, [X2, Y2]], Type),
+    (Type = object; Type = cell),
     same_cell([Elem1, [X1, Y1]], [Elem2, [X2, Y2]]).
 
+% Combined danger/cell check for efficiency
 distance([Elem1, [X1, Y1]], [Elem2, [X2, Y2]]):-
-    (
-        data_concept([Elem2, [X2, Y2]], danger);
-        data_concept([Elem2, [X2, Y2]], cell)
-    ),
+    data_concept([Elem2, [X2, Y2]], Type),
+    (Type = danger; Type = cell),
     adjacent_cell([Elem1, [X1, Y1]], [Elem2, [X2, Y2]]).
 
-
 same_cell([Elem1, [X1, Y1]], [Elem2, [X2, Y2]]):-
-    Elem1 \= Elem2, X1 is X2, Y1 is Y2.
+    Elem1 \= Elem2, 
+    X1 is X2, 
+    Y1 is Y2.
 
-
+% Optimized adjacent_cell using delta positions
 adjacent_cell([Elem1, [X1, Y1]], [Elem2, [X2, Y2]]):-
     Elem1 \= Elem2,
-    (
-        X1 is X2, Y1 is Y2 + 1;
-        X1 is X2, Y1 is Y2 - 1;
-        X1 is X2 + 1, Y1 is Y2;
-        X1 is X2 - 1, Y1 is Y2
-    ).
+    Delta = [[0,1], [0,-1], [1,0], [-1,0]],
+    member([DX,DY], Delta),
+    X1 is X2 + DX,
+    Y1 is Y2 + DY.
