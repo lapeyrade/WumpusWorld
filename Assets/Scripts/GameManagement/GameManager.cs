6341f9797a4c2e0b5ce73897d0ef4df619692a25
Sylvain Lapeyrade <sylvain.lapeyrade@hotmail.fr>
2025-04-07 13:45:31 +0900

    fix json result files inconsistencies

--- Assets/Scripts/GameManagement/GameManager.cs
+++ Assets/Scripts/GameManagement/GameManager.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Prolog;
 using UnityEngine;
 using Random = UnityEngine.Random;
-using System.Linq;
 
 namespace GameManagement
 {
